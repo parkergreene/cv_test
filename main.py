bea9f2c43f92deb36a425fcbcad0a66ef1bfecb5
Parker Greene <parkergreene16@gmail.com>
2022-09-23 23:49:02 -0700

    new stuff lol

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("hello world")
+import cv2 as cv
+import face_recognition
+
+webcam = cv.VideoCapture(0)
+
+face_locations = []
+
+trained_data = cv.CascadeClassifier('frontal-face-data.xml')
+
+
+
+
+
+while True:
+    check, frame = webcam.read()
+
+    rgb_frame = frame[:, :, ::-1]
+
+    face_locations = face_recognition.face_locations(rgb_frame)
+
+    cv.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2) 
+
+    cv.imshow('video', frame)
+
+    key = cv.waitKey(1)
+    if key == ord('e'):
+        break
+
+webcam.release()
+cv.destroyAllWindows()
